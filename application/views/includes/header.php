9e7a3c22704329c0188ff21ab5b2a63d8b05cc88
Renjith NKS <renjithnks@gmail.com>
2021-01-18 10:15:47 +0530

    CLS Fixed CSS Reverted

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -33,7 +33,7 @@
 <!-- need a change  -->
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-115799470-1"></script>
+<!--<script async src="https://www.googletagmanager.com/gtag/js?id=UA-115799470-1"></script>-->
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
